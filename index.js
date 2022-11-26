de651e6b8f241a767766d914a7b70b6ad0708fc2
Jon Poulsen <jnpoulsen@live.dk>
2022-11-26 18:58:46 +0100

    comment out database connection

--- index.js
+++ index.js
@@ -13,45 +13,45 @@ app.use(express.static(__dirname));
 
 /* ----- IMPORTERING AF QUESTIONS-TABEL ----- */
 
-const dbConfig = require('./config/config');
-const { Sequelize, Model } = require('sequelize'); 
+// const dbConfig = require('./config/config');
+// const { Sequelize, Model } = require('sequelize'); 
 
-const sequelize = new Sequelize(
-        dbConfig.DATABASE,
-        dbConfig.USER,
-        dbConfig.PASSWORD,
-        {
-          host: dbConfig.HOST,
-          dialect: dbConfig.DIALECT
-        }
-    );
+// const sequelize = new Sequelize(
+//         dbConfig.DATABASE,
+//         dbConfig.USER,
+//         dbConfig.PASSWORD,
+//         {
+//           host: dbConfig.HOST,
+//           dialect: dbConfig.DIALECT
+//         }
+//     );
 
-const db = {};
-db.sequelize = sequelize;
-db.models = {};
-db.models.Question = require('./models/questions')(sequelize, Sequelize.DataTypes);
+// const db = {};
+// db.sequelize = sequelize;
+// db.models = {};
+// db.models.Question = require('./models/questions')(sequelize, Sequelize.DataTypes);
 
-module.exports = db;
+// module.exports = db;
 
-sequelize.authenticate().then(() => {
-   console.log('Connection has been established successfully.');
-}).catch((error) => {
-   console.error('Unable to connect to the database: ', error);
-});
+// sequelize.authenticate().then(() => {
+//    console.log('Connection has been established successfully.');
+// }).catch((error) => {
+//    console.error('Unable to connect to the database: ', error);
+// });
 
-const Question = require('./models/questions')(sequelize)
-console.log(Question === sequelize.models.Question); //eksport virker
+// const Question = require('./models/questions')(sequelize)
+// console.log(Question === sequelize.models.Question); //eksport virker
 
-(async () => {
-	await db.sequelize.sync().then(() => 
-	{
-		console.log('Table has been synchronized.');
-	}
-	).catch((error) => 
-	{
-		console.log('Synchronization failed: ', error)
-	});
-	})();
+// (async () => {
+// 	await db.sequelize.sync().then(() => 
+// 	{
+// 		console.log('Table has been synchronized.');
+// 	}
+// 	).catch((error) => 
+// 	{
+// 		console.log('Synchronization failed: ', error)
+// 	});
+// 	})();
 
 /*----- GET METHODS -----*/
 
--- question_controller.js
+++ question_controller.js
@@ -1,49 +1,49 @@
 //måske bør denne fil slettes - opgaverne er uddelegeret til index.js
 
-const Sequelize = require('sequelize'); 
+// const Sequelize = require('sequelize'); 
 
-const sequelize = new Sequelize( //https://www.digitalocean.com/community/tutorials/how-to-use-sequelize-with-node-js-and-mysql
-        'udelukket', //database
-        'root', //Jon?
-        'Qcumber',
-        {
-          host: 'localhost',
-          dialect: 'mysql'
-        }
-    );
+// const sequelize = new Sequelize( //https://www.digitalocean.com/community/tutorials/how-to-use-sequelize-with-node-js-and-mysql
+//         'udelukket', //database
+//         'root', //Jon?
+//         'Qcumber',
+//         {
+//           host: 'localhost',
+//           dialect: 'mysql'
+//         }
+//     );
 
-sequelize.authenticate().then(() => {
-   console.log('Connection has been established successfully.');
-}).catch((error) => {
-   console.error('Unable to connect to the database: ', error);
-});
+// sequelize.authenticate().then(() => {
+//    console.log('Connection has been established successfully.');
+// }).catch((error) => {
+//    console.error('Unable to connect to the database: ', error);
+// });
 
-const Question = require('./questions')(sequelize)
+// const Question = require('./questions')(sequelize)
 
-sequelize.sync().then(() => {
-console.log('Question table created successfully!');
+// sequelize.sync().then(() => {
+// console.log('Question table created successfully!');
 
-var rowCount = 0;
-Question.count({ //HOW TO RESOLVE??
-  where: { question_ID: "1" }
-});
+// var rowCount = 0;
+// Question.count({ //HOW TO RESOLVE??
+//   where: { question_ID: "1" }
+// });
 
-var randomNumber = Math.floor(Math.random() * rowCount) + 1;
-var randomStr = randomNumber.toString();
+// var randomNumber = Math.floor(Math.random() * rowCount) + 1;
+// var randomStr = randomNumber.toString();
 
-console.log(rowCount);
+// console.log(rowCount);
 
-var result = 3;
-result = Question.findOne({
-    where: {
-        question_ID : 1 //SKAL VÆRE randomStr, NÅR DENNE BEREGNES RIGTIGT
-    }
-}).then(res => {
-    console.log(res.dataValues.question_text);
-}).catch((error) => {
-    console.error('Failed to retrieve data : ', error);
-});
+// var result = 3;
+// result = Question.findOne({
+//     where: {
+//         question_ID : 1 //SKAL VÆRE randomStr, NÅR DENNE BEREGNES RIGTIGT
+//     }
+// }).then(res => {
+//     console.log(res.dataValues.question_text);
+// }).catch((error) => {
+//     console.error('Failed to retrieve data : ', error);
+// });
 
-}).catch((error) => {
-    console.error('Unable to create table : ', error);
-});
+// }).catch((error) => {
+//     console.error('Unable to create table : ', error);
+// });
